d65edcfe3cd6684fd531e30b386f7c94aa627390
Norly Pierre <norlypierre@gmail.com>
2025-12-09 01:15:27 -0500

    LeaseDao -> Try-With-Resources add & remove Methods (JDBC)

--- src/main/java/com/pluralsight/dealership/db/InventoryDao.java
+++ src/main/java/com/pluralsight/dealership/db/InventoryDao.java
@@ -6,17 +6,43 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class InventoryDao {
-    private DataSource dataSource;
+    private final DataSource dataSource;
 
     public InventoryDao(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
     public void addVehicleToInventory(String vin, int dealershipId) {
-        // TODO: Implement the logic to add a vehicle to the inventory
+
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement ps = conn.prepareStatement(
+                     "INSERT INTO inventory (vin, dealership_id) VALUES (?, ?)")) {
+
+            ps.setString(1, vin);
+            ps.setInt(2, dealershipId);
+
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(
+                    "Error adding Vehicle " + vin + " to Inventory for Dealership " + dealershipId, e
+            );
+        }
     }
 
     public void removeVehicleFromInventory(String vin) {
-        // TODO: Implement the logic to remove a vehicle from the inventory
+
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement ps = conn.prepareStatement(
+                     "DELETE FROM inventory WHERE vin = ?")) {
+
+            ps.setString(1, vin);
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(
+                    "Error removing Vehicle " + vin + " from Inventory", e
+            );
+        }
     }
 }
