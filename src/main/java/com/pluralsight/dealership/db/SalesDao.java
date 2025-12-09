c8fad332af1c0fb0eeb23f6e1429edbd78c0ec83
Norly Pierre <norlypierre@gmail.com>
2025-12-09 01:13:15 -0500

    SalesContract -> Try-With-Resources add Methods (JDBC)

--- src/main/java/com/pluralsight/dealership/db/SalesDao.java
+++ src/main/java/com/pluralsight/dealership/db/SalesDao.java
@@ -3,15 +3,34 @@ package com.pluralsight.dealership.db;
 import com.pluralsight.dealership.models.SalesContract;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class SalesDao {
-    private DataSource dataSource;
+    private final DataSource dataSource;
 
     public SalesDao(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
     public void addSalesContract(SalesContract salesContract) {
-        // TODO: Implement the logic to add a sales contract
+
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement ps = conn.prepareStatement(
+                     "INSERT INTO sales_contracts (vin, sale_date, price) " +
+                             "VALUES (?, ?, ?)")) {
+
+            ps.setString(1, salesContract.getVin());
+            ps.setDate(2, Date.valueOf(salesContract.getSaleDate()));
+            ps.setDouble(3, salesContract.getPrice());
+
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Error adding Sales Contract for VIN: "
+                    + salesContract.getVin(), e);
+        }
     }
 }
